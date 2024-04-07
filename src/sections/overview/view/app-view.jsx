15a857b510f15e34a60f7e63340d27119e1e728b
HyeonYul <gusdbf1114@gmail.com>
2024-04-07 19:42:22 +0900

    first

---
+++ src/sections/overview/view/app-view.jsx
@@ -0,0 +1,168 @@
+import { faker } from '@faker-js/faker';
+
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+
+import AppNewsUpdate from '../app-news-update';
+import AppOrderTimeline from '../app-order-timeline';
+import AppCurrentVisits from '../app-current-visits';
+import AppWidgetSummary from '../app-widget-summary';
+import AppCurrentSubject from '../app-current-subject';
+import AppConversionRates from '../app-conversion-rates';
+
+// ----------------------------------------------------------------------
+
+export default function AppView() {
+  return (
+    <Container maxWidth="xl">
+      <Typography variant="h4" sx={{ mb: 5 }}>
+        Retire Calculator 👋
+      </Typography>
+
+      <Grid container spacing={3}>
+        <Grid xs={12} sm={6} md={2.4}>
+          <AppWidgetSummary
+            title="초봉"
+            total={714000}
+            color="success"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={2.4}>
+          <AppWidgetSummary
+            title="저축률"
+            total={1352831}
+            color="info"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={2.4}>
+          <AppWidgetSummary
+            title="투자수익률"
+            total={1723315}
+            color="warning"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={2.4}>
+          <AppWidgetSummary
+            title="근무기간"
+            total={1723315}
+            color="warning"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+          />
+        </Grid>
+
+
+        <Grid xs={12} sm={6} md={2.4}>
+          <AppWidgetSummary
+            title="국민연금"
+            total={234}
+            color="error"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={8}>
+        <AppCurrentVisits
+            title="Current Visits"
+            chart={{
+              series: [
+                { label: 'America', value: 4344 },
+                { label: 'Asia', value: 5435 },
+                { label: 'Europe', value: 1443 },
+                { label: 'Africa', value: 4443 },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppCurrentVisits
+            title="Current Visits"
+            chart={{
+              series: [
+                { label: 'America', value: 4344 },
+                { label: 'Asia', value: 5435 },
+                { label: 'Europe', value: 1443 },
+                { label: 'Africa', value: 4443 },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={8}>
+          <AppConversionRates
+            title="Conversion Rates"
+            subheader="(+43%) than last year"
+            chart={{
+              series: [
+                { label: 'Italy', value: 400 },
+                { label: 'Japan', value: 430 },
+                { label: 'China', value: 448 },
+                { label: 'Canada', value: 470 },
+                { label: 'France', value: 540 },
+                { label: 'Germany', value: 580 },
+                { label: 'South Korea', value: 690 },
+                { label: 'Netherlands', value: 1100 },
+                { label: 'United States', value: 1200 },
+                { label: 'United Kingdom', value: 1380 },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppCurrentSubject
+            title="Current Subject"
+            chart={{
+              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
+              series: [
+                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
+                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
+                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={8}>
+          <AppNewsUpdate
+            title="News Update"
+            list={[...Array(5)].map((_, index) => ({
+              id: faker.string.uuid(),
+              title: faker.person.jobTitle(),
+              description: faker.commerce.productDescription(),
+              image: `/assets/images/covers/cover_${index + 1}.jpg`,
+              postedAt: faker.date.recent(),
+            }))}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppOrderTimeline
+            title="Order Timeline"
+            list={[...Array(5)].map((_, index) => ({
+              id: faker.string.uuid(),
+              title: [
+                '1983, orders, $4220',
+                '12 Invoices have been paid',
+                'Order #37745 from September',
+                'New order placed #XF-2356',
+                'New order placed #XF-2346',
+              ][index],
+              type: `order${index + 1}`,
+              time: faker.date.past(),
+            }))}
+          />
+        </Grid>
+
+    
+      </Grid>
+    </Container>
+  );
+}
